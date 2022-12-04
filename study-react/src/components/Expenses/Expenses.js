d4d6e46205a7e9d6352177c0420d7a0974454f8c
dev1week <rondo2860@gmail.com>
2022-12-04 12:49:44 +0900

    제출 양식 수정

--- study-react/src/components/Expenses/Expenses.js
+++ study-react/src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
@@ -26,6 +26,6 @@ function Expenses(props) {
       />
     </Card>
   );
-}
+};
 
 export default Expenses;
